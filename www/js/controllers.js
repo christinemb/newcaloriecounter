887514979a7ff1b4fc00008bbaf99dedb4e00d8f
unknown <wattersonglendon@yahoo.co.uk>
2015-04-29 21:35:35 +0100

    changes to controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,73 @@
 angular.module('newCalorieCounter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
+.controller('AppCtrl', function($scope) {
+ $scope.categories = ['Meat', 'Vegetable', 'Fruit', 'Snacks'];
   
 
    
 })
 
 .controller('foodsCtrl', function($scope, $stateParams) {
+categories = ['Meat', 'Vegetable', 'Fruit', 'Fish&Poultry' , 'Drinks'];
+    
+    foodlists = [
+    [
+     "back bacon",
+      "beef fillet",
+      "pork sausage",
+      "pork chop",
+       "pork fillet",
+      "roast beef",
+      "roast lamb",
+      "sirlion steak",
+       "streaky bacon"
+    ],
+    [
+      "asparagus",
+      "broccoli",
+      "onion",
+      "cabbage",
+      "carrot",
+      "courgette",
+      "greenbeans",
+      "parsnip",
+      "red pepper",
+       "tomato",
+    ],
+    [
+      "apple",
+      "banana",
+      "blueberries",
+      "kiwi",
+     "orange",
+      "peach",
+      "pear",
+      "pinepple",
+     "strawberry",
 
+   ],
+   [
+     "muffin",
+      "sweets",
+      "chocolate",
+     "lollipop",
+      "popcorn",
+      "crisps",
+      "toffee",
+     "cake",
+        ],
+    [
+       
+    
+     
+ 
+ ];
+     
+     
+
+ $scope.category = categories[$stateParams.foodsId];
+    $scope.foods = foods[$stateParams.foodlistsId];
+ });
+ 
 $scope.dishesId = $stateParams.dishesId;
 });
